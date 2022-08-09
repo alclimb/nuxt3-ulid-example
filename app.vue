ced56a97e468b5f317def8699edb76e63016ad6a
hikaru <alclimb@users.noreply.github.com>
2022-08-09 16:12:03 +0900

    feat: ダイナミックインポートでulidを使用

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
+<script lang="ts" setup>
+// import { ulid } from "ulid";
+
+async function onClick() {
+  const { ulid } = await import(`ulid`);
+  console.log(ulid());
+}
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <button @click="onClick">button1</button>
   </div>
 </template>
